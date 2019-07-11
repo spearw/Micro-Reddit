6813b093fad93af051dd73df18986a07c759c94f
William Spear <spearw@gmail.com>
2019-07-11 14:31:51 -0400

    configure to use postgres instead of sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_07_09_201651) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
