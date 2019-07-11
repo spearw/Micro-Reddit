997b61968ec7238d2595294813262966348b687c
William Spear <spearw@gmail.com>
2019-07-11 15:48:30 -0400

    user signup form and sign in form added to nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+
+  get 'sessions/new'
   get 'users/new'
   get 'homepage/index'
+
   get  '/signup',  to: 'users#new'
   post '/signup', to: 'users#show'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
   resources :posts do
